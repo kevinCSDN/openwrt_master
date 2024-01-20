7d741660812f3df994e1e40e40223414e5e853bf
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-21 04:44:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,20 +9,21 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-#删除feeds自带mosdns、v2ray-geodata
-rm -rf ./feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
+#删除feeds自带luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon
 
+#克隆luci-theme-argon from jerrykuku
 git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
+#克隆luci-app-netdata&luci-app-smartdns&luci-app-upnp
 git clone https://github.com/kiddin9/openwrt-packages
 shopt -s extglob
 rm -rf openwrt-packages/!(luci-app-netdata|luci-app-smartdns|luci-app-upnp)
 cp -r openwrt-packages/{luci-app-netdata,luci-app-smartdns,luci-app-upnp} package/
 rm -rf openwrt-packages
 
+#克隆定时重启
 cd package
 git clone https://github.com/f8q8/luci-app-autoreboot.git 
 cd ..
